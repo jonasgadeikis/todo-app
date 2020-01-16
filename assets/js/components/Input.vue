0703f7eefd0ade6ce16faac581842b9eb431036d
jonasgadeikis <jonasgadeikis.github@gmail.com>
2020-01-16 20:09:59 +0200

    Create Base Components

---
+++ assets/js/components/Input.vue
@@ -0,0 +1,26 @@
+<template>
+    <input
+        :type="type"
+        :class="classes"
+        :placeholder="placeholder"
+        v-model="inputModel"
+        @input="$emit('update:model', inputModel)"
+    >
+</template>
+
+<script>
+    export default {
+        name: 'Input',
+        props: {
+            type: String,
+            classes: String,
+            placeholder: String,
+            model: String,
+        },
+        data() {
+            return {
+                inputModel: '',
+            }
+        },
+    }
+</script>
